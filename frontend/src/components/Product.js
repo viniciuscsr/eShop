817e0c52971a2674d2a79005719cfbed2e619627
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2021-01-02 18:19:56 -0500

    product card design change

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,7 +5,7 @@ import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card style={{ border: 'none' }} className='my-3 p-3'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
